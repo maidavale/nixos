f7c75f6c18e1f0216b1eab2a56bf3bd133fae81f
Martijn Rats <mrats@pm.me>
2025-12-26 16:29:19 +0000

    Install Librewolf on Delft

--- modules/nixos/packages-delft.nix
+++ modules/nixos/packages-delft.nix
@@ -8,18 +8,13 @@ let
     ];
   };
 
-  myChromiumX11 = pkgs.writeShellScriptBin "chromium-x11" ''
-    export QT_QPA_PLATFORM=xcb
-    export XDG_SESSION_TYPE=x11
-    exec ${pkgs.ungoogled-chromium}/bin/chromium --ozone-platform=x11 "$@"
-  '';
 in
 {
   environment.systemPackages = with pkgs; [
         
     # browsers
     brave
-    myChromiumX11
+    librewolf
     
     # desktop
     gruvbox-gtk-theme

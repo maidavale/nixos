fe43bfc77ac7111a65e3e70e7b4719fe1eef0036
Martijn Rats <mrats@pm.me>
2025-12-26 14:17:14 +0000

    Updated Starship prompt

--- home/martijn/dotfiles/starship.nix
+++ home/martijn/dotfiles/starship.nix
@@ -3,11 +3,30 @@
 {
   home.file.".config/starship.toml" = {
     text = ''
-      format = """ $directory $character"""
+      format = """
+        $directory$git_branch$git_status
+        $character
+      """
 
       [directory]
       style = "bold blue"
       truncation_length = 3
+
+      [git_branch]
+      format = "[$symbol$branch]($style)"
+      style = "bold purple"
+      symbol = " "
+
+      [git_status]
+      format = "[($all_status$ahead_behind )]($style)"
+      style = "bold yellow"
+      conflicted = "✖"
+      deleted = "✘"
+      modified = "✚"
+      renamed = "»"
+      staged = "●"
+      stashed = "≡"
+      untracked = "?"
     '';
   };
 }

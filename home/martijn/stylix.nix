30f6576cf7c13db7743f7a33ad9215b5a0b6e2bc
Martijn Rats <mrats@pm.me>
2025-12-31 15:43:18 +0000

    Improving clarity in Stylix settings

--- home/martijn/stylix.nix
+++ home/martijn/stylix.nix
@@ -2,11 +2,10 @@
 
 {
   stylix = {
-    image = "${toString config._module.args.flakeRoot}/wallpapers/wallpaper_linux.jpg";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
     targets.firefox.enable = true;
     targets.kitty.enable = true;
+
   };
 }
 
--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -4,9 +4,7 @@
   stylix = {
     enable = true;
 
-    # modules/nixos/stylix.nix -> ../../../wallpapers/...
     image = "${toString config._module.args.flakeRoot}/wallpapers/wallpaper_linux.jpg";
-
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
     targets.gnome.enable = true;
